1d173e8773874c88b6acc69878e472745c0ab07b
Shane Holloway <shane.holloway@ieee.org>
2018-12-21 13:29:04 -0700

    v0.3.0 updating to jsy-transpile@0.5.8 and fx@10.0.0

--- src/_fx_index.js
+++ src/_fx_index.js
@@ -1,47 +1,64 @@
 // Forked under MIT license from https://github.com/antonmedv/fx/blob/961ee629f416ea92ec5eef1e0f04a5984a5d1649/index.js
 
 'use strict'
-const pretty = require('@medv/prettyjson')
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const {stdin, stdout, stderr} = process
+try {
+  require(path.join(os.homedir(), '.fxrc'))
+} catch (err) {
+  if (err.code !== 'MODULE_NOT_FOUND') {
+    throw err
+  }
+}
+
+const print = require('fx/print.js')
+
 
 function main(input, {usage, reducer}) {
-  const {stdout, stderr} = process
+  let args = process.argv.slice(2)
+  let filename = 'fx'
 
   if (input === '') {
-    stderr.write(usage)
-    process.exit(2)
+    if (args.length === 0) {
+      stderr.write(usage)
+      process.exit(2)
+    }
+
+    input = fs.readFileSync(args[0])
+    filename = path.basename(args[0])
+    args = args.slice(1)
   }
 
   const json = JSON.parse(input)
-  const args = process.argv.slice(2)
 
   if (args.length === 0 && stdout.isTTY) {
-    require('fx/fx')(json)
+    require('fx/fx')(filename, json)
     return
   }
 
-  const result = args.reduce(reducer, json)
+  const output = args.reduce(reducer, json)
 
-  if (typeof result === 'undefined') {
+  if (typeof output === 'undefined') {
     stderr.write('undefined\n')
-  } else if (typeof result === 'string') {
-    console.log(result)
-  } else if (stdout.isTTY) {
-    console.log(pretty(result))
+  } else if (typeof output === 'string') {
+    console.log(output)
   } else {
-    console.log(JSON.stringify(result, null, 2))
+    const [text] = print(output)
+    console.log(text)
   }
 }
 
 function run(options) {
-  const stdin = process.stdin
   stdin.setEncoding('utf8')
-
-  let buff = ''
 
   if (stdin.isTTY) {
     main('', options)
+    return
   }
 
+  let buff = ''
   stdin.on('readable', () => {
     let chunk
 
